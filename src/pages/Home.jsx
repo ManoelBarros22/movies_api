3e58cf7efa5a2cae95579cc386eb0f9eafaef78f
ManoelBarros22 <126240510+ManoelBarros22@users.noreply.github.com>
2023-12-12 21:36:25 -0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+  };
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+        getTopRatedMovies(topRatedUrl)
+    }, []);
+  
+
+  return <div>Home</div>
+  
+}
+
+export default Home
